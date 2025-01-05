0e47be9387732100a8305316b3c03ef71aa19f00
WANDAVISION\leemf <dd20150224@gmail.com>
2025-01-05 16:06:13 +0800

    changed to test 6-1

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
-# test 6
+# test 6-1
 
 
 def create_app(db_url=None):
